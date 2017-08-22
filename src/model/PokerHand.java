c6fb3ac1f9bce55fa452e6218541c8f2c4948d92
Rick Mercer <rhm12399@users.noreply.github.com>
2017-08-22 09:54:44 -0700

    Import starter files
    The src folder has two packages: model and tests.  Package model has two enums, class Card, and the start of a unit test for PokerHand

---
+++ src/model/PokerHand.java
@@ -0,0 +1,18 @@
+package model;
+
+// Model a five card PokerHand that is constructed with five Card objects
+//
+// PokerHand a = new PokerHand(H3, CA, D4, H6, DA);
+//
+public class PokerHand implements Comparable<PokerHand> {
+
+  // Construct a five card poker hand.
+  public PokerHand(Card c1, Card c2, Card c3, Card c4, Card c5) throws DuplicateCardException {
+  }
+
+  @Override
+  public int compareTo(PokerHand o) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+}
